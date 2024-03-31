244accb92063b281ca8f763500bec3973427047b
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-31 11:41:25 +0200

    Added nextSlide/prevSlide methods

--- js/script.js
+++ js/script.js
@@ -6,10 +6,25 @@ createApp({
     data(){
         return {
             slides: slides,
+            activeIndexSlide: 0 // l'indice della slide attiva va inserito dentro una variabile in modo da poter scorrere il resto delle immagini */
         }
     },
     methods: {
-
+        nextSlide(){
+            if(this.activeIndexSlide < this.slides.length -1){
+                this.activeIndexSlide++; // incremento di uno la slide attiva
+        }else {
+            this.activeIndexSlide = 0;
+        }
+    },
+        prevSlide(){
+            if(this.activeIndexSlide > 0){
+            this.activeIndexSlide--; //decremento di uno la slide attiva
+        } else {
+            this.activeIndexSlide = this.slides.length - 1;
+        }
+        
+        }
     },
     mounted(){
 
